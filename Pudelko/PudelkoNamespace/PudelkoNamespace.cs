4526eb7f3a071fdfaa73eb98ba6af536e46b9d62
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2023-04-27 16:02:15 +0200

    adding constructor and exceptions

--- Pudelko/PudelkoNamespace/PudelkoNamespace.cs
+++ Pudelko/PudelkoNamespace/PudelkoNamespace.cs
@@ -15,19 +15,20 @@ namespace PudelkoNamespace.PudelkoLib
         private readonly double _c;
 
         public UnitOfMeasure Measure { get; set; }
-        
+
+
         // meter returning function "ReturnMeters"
         public static double ReturnMeters(double value, UnitOfMeasure m)
         {
             if (m == UnitOfMeasure.meter)
             {
-                return Math.Round(value,14);
+                return Math.Round(value, 14);
             }
             else if (m == UnitOfMeasure.milimeter)
             {
                 if (value < 1) return 0;
 
-                return Math.Round(value / 1000,14);
+                return Math.Round(value / 1000, 14);
             }
             else if (m == UnitOfMeasure.centimeter)
             {
@@ -37,12 +38,29 @@ namespace PudelkoNamespace.PudelkoLib
             }
             else throw new FormatException("Invalid type for MeasureType enum.");
         }
-        
-        
+        // constructor with a,b,c, type of measure and exceptions
+        public Pudelko(double a, double b, double c, UnitOfMeasure type)
+        {
+            if (!Enum.IsDefined(typeof(UnitOfMeasure), type))
+            {
+                throw new FormatException("Invalid type for MeasureType enum.");
+            }
+            if (ReturnMeters(a, type) <= 0 || ReturnMeters(b, type) <= 0 || ReturnMeters(c, type) <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Dimensions cannot be negative or equal to 0");
+            }
+
+            if (ReturnMeters(a,type) > 10 || ReturnMeters(b, type) > 10 || ReturnMeters(c, type) > 10) {
+                throw new ArgumentOutOfRangeException("The maximum dimensions of the box are 10m x 10m x 10m");
+            }
+            _a = a;
+            _b = b;
+            _c = c;
+            Measure = type;
+        }
+
+
+
     }
-    
-
-
-
 
 }
